cc13c4ca5d2a65ea61aa5e1c639d7e218c312419
Patrick Moon <pmoon@letsgolearn.com>
2020-07-03 18:58:33 -0700

    Dealign with display issues

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
-<?php require "start.php"; ?>
+<?php
+require 'vendor/autoload.php';
+require 'config.php';
+require 'functions.php';
+$title= $title ?? 'test';
 
+new Models\Database();
+new Models\Users();
 
-<?php require_once ('end.php'); ?>
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false){
+
+    header("location: login.php");
+}
+
+$thisCluster = (object) [
+    'title' => \Models\Clusters::find($_SESSION["cluster_id"])->title,
+    'id' => $_SESSION["cluster_id"]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?=$title;?></title>
+    <script src="https://kit.fontawesome.com/b61a9642d4.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link href="public/css/default.css" type="text/css" rel="stylesheet">
+    <script src="public/js/default.js"></script>
+    <script>
+        if(localStorage.getItem("clusterId") === undefined) {
+            localStorage.setItem("clusterId", 1);
+        }
+    </script>
+</head>
+<body>
+<H1>SETradingHouse</H1>
+<?php require_once('menubar.php'); ?>
+
